07aa8e9fcbc20dcbe8e953f46203d3cf8c4a6e3e
Mutsuha Asada <me@momee.mt>
2024-05-09 12:32:25 +0900

    init: time-series-prediction-of-deterministic-chaos

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = import nixpkgs {inherit system;};
+      in {
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            nil
+            alejandra
+            python311
+            ruff
+            python311Packages.numpy
+            python311Packages.matplotlib
+          ];
+        };
+      }
+    );
+}
